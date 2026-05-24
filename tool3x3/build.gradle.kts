b5b7f6168845736c7c8fd38b908548dc4247624b
Hyakjbw <123542925+Hyakjbw@users.noreply.github.com>
2026-05-24 20:18:09 +0700

    Update build.gradle.kts

--- tool3x3/build.gradle.kts
+++ tool3x3/build.gradle.kts
@@ -3,15 +3,17 @@ plugins {
 }
 
 group = "com.example"
-version = "1.0.0"
+version = "1.0.1" // tăng version nhẹ
 
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://maven.enginehub.org/repo/") // WorldGuard
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9")
 }
 
 java {
