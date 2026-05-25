d1e121f1827aa0692f038569f8cdf6c6ba94ff79
Hyakjbw <123542925+Hyakjbw@users.noreply.github.com>
2026-05-25 09:01:13 +0700

    Update ExcavatorTools.java

--- tool3x3/src/main/java/com/example/excavatortools/ExcavatorTools.java
+++ tool3x3/src/main/java/com/example/excavatortools/ExcavatorTools.java
@@ -1,28 +1,44 @@
 package com.example.excavatortools;
 
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ExcavatorTools extends JavaPlugin {
 
     private static ExcavatorTools instance;
+    private static NamespacedKey excavatorKey;
+    private ConfigManager configManager;
 
     @Override
     public void onEnable() {
         instance = this;
-        WorldGuardHook.init();
+        excavatorKey = new NamespacedKey(this, "excavator");
+        configManager = new ConfigManager(this);
+        WorldGuardHook.init(); // nếu có
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
         getCommand("excavatortool").setExecutor(new ExcavatorCommand());
-        getLogger().info("ExcavatorTools enabled!");
+        getLogger().info("ExcavatorTools enabled! Debug: " + configManager.isDebug());
     }
 
     @Override
     public void onDisable() {
-        // Hủy đăng ký sự kiện và lệnh (Bukkit tự làm, nhưng ta đặt instance = null để GC)
         instance = null;
-        getLogger().info("ExcavatorTools disabled.");
     }
 
-    public static ExcavatorTools getInstance() {
-        return instance;
+    public static ExcavatorTools getInstance() { return instance; }
+    public static NamespacedKey getExcavatorKey() { return excavatorKey; }
+    public ConfigManager getConfigManager() { return configManager; }
+
+    public static boolean isExcavatorTool(ItemStack item) {
+        if (item == null || !item.hasItemMeta()) return false;
+        return item.getItemMeta().getPersistentDataContainer().has(getExcavatorKey(), PersistentDataType.BYTE);
+    }
+
+    public void debug(String message) {
+        if (configManager.isDebug()) {
+            getLogger().info("[DEBUG] " + message);
+        }
     }
 }
