4fd91335b27f70c543c89917e913157d196ef0f7
Hyakjbw <123542925+Hyakjbw@users.noreply.github.com>
2026-06-21 17:42:59 +0700

    Update RamOptMod.java

--- ram-opt-1.20.1/src/main/java/com/hyakjbw/ramopt/RamOptMod.java
+++ ram-opt-1.20.1/src/main/java/com/hyakjbw/ramopt/RamOptMod.java
@@ -4,8 +4,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.ModLoadingContext;
-import com.hyakjbw.ramopt.events.RamHudOverlay;
-import com.hyakjbw.ramopt.events.RamCleaner;
 
 // Tên modid phải khớp chính xác với khai báo trong mods.toml và build.gradle
 @Mod("ramopt")
