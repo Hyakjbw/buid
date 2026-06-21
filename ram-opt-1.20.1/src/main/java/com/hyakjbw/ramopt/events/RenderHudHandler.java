f0ad2b5eab68b941730fed400d041e0ea81f12bb
Hyakjbw <123542925+Hyakjbw@users.noreply.github.com>
2026-06-21 18:28:33 +0700

    fix: Use correct RenderGuiEvent.Post for HUD rendering in 1.20.1

--- ram-opt-1.20.1/src/main/java/com/hyakjbw/ramopt/events/RenderHudHandler.java
+++ ram-opt-1.20.1/src/main/java/com/hyakjbw/ramopt/events/RenderHudHandler.java
@@ -2,7 +2,7 @@ package com.hyakjbw.ramopt.events;
 
 import com.hyakjbw.ramopt.service.HudRenderer;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ScreenEvent;
+import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -15,7 +15,7 @@ public class RenderHudHandler {
     private static final HudRenderer HUD_RENDERER = HudRenderer.getInstance();
 
     @SubscribeEvent
-    public static void onScreenRender(ScreenEvent.Init.Post event) {
+    public static void onRenderGuiOverlay(RenderGuiEvent.Post event) {
         try {
             HUD_RENDERER.renderHud(event.getGuiGraphics());
         } catch (Exception e) {
